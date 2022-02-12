0ef7f4b918548ebd61039e7c7975862751041e81
Furkan <f.ceylan1@gmx.de>
2022-02-12 12:14:13 +0100

    image fix

--- js/popup.js
+++ js/popup.js
@@ -3,3 +3,9 @@ function toggle(target) {
   let popup = document.querySelector(target); // get id from html target (onclick)
   popup.classList.toggle("active"); // if html element has onclick="toggle" -> activate active class in css
 }
+
+let video = document.getElementById("video");
+function stopVideo() {
+  video.pause();
+  video.currentTime = 0;
+}
